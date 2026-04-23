7d35b86cd83b7aadc1852218221b512cbd290e2c
Silvio Duarte <silviomessiaspa@gmail.com>
2026-04-23 09:59:00 -0300

    fix: refatora url_opener_web usando package:web para compatibilidade WASM

--- lib/url_opener_web.dart
+++ lib/url_opener_web.dart
@@ -1,31 +1,7 @@
-import 'dart:js_interop';
-
-@JS('window.open')
-external void _windowOpen(String url, String target);
-
-@JS('document.createElement')
-external JSObject _createElement(String tagName);
-
-extension type HTMLAnchorElement(JSObject _) implements JSObject {
-  @JS('href')
-  external set href(String value);
-  @JS('download')
-  external set download(String value);
-  @JS('target')
-  external set target(String value);
-  @JS('click')
-  external void click();
-  @JS('remove')
-  external void remove();
-  @JS('setAttribute')
-  external void setAttribute(String name, String value);
-}
-
-@JS('document.body.append')
-external void _bodyAppend(JSObject element);
+import 'package:web/web.dart' as web;
 
 Future<void> openExternal(String url) async {
-  _windowOpen(url, '_blank');
+  web.window.open(url, '_blank');
 }
 
 String assetPublicUrl(String assetKey) {
@@ -36,24 +12,23 @@ String assetPublicUrl(String assetKey) {
 
 Future<void> downloadAsset(String assetKey, {String? filename}) async {
   final url = assetPublicUrl(assetKey);
-  final anchor = HTMLAnchorElement(_createElement('a'))
+  final anchor = web.HTMLAnchorElement()
     ..href = url
     ..download = filename ?? assetKey.split('/').last
     ..target = '_blank';
-  _bodyAppend(anchor);
+  web.document.body?.append(anchor);
   anchor.click();
   anchor.remove();
 }
 
 Future<void> downloadFile(String url, {String? filename}) async {
-  final anchor = HTMLAnchorElement(_createElement('a'))
-    ..href = url;
+  final anchor = web.HTMLAnchorElement()..href = url;
   if (filename != null && filename.isNotEmpty) {
     anchor.download = filename;
   } else {
     anchor.setAttribute('download', '');
   }
-  _bodyAppend(anchor);
+  web.document.body?.append(anchor);
   anchor.click();
   anchor.remove();
 }
